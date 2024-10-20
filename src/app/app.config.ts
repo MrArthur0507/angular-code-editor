b2bedb6483fbc105707b2a4bbfb7c13344b59223
Bozhidar <mrarthur0507@gmail.com>
2024-10-20 15:49:51 +0300

    configured ngx-monaco-editor dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
+import { provideMonacoEditor } from 'ngx-monaco-editor-v2'
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideMonacoEditor()]
 };
+
+
